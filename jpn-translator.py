9e3e307b3d45dbd64e9ada7b23e56b61be0ddd67
Manthan Awgan <manthanawgan@gmail.com>
2025-07-27 22:46:34 +0530

    yo

--- jpn-translator.py
+++ jpn-translator.py
@@ -1,12 +1,16 @@
 import easyocr
 import cv2
+import sys
+import io
+from google import genai
+from google.genai import types
 
 
-
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 reader = easyocr.Reader(['ja', 'en'])
 
-result = reader.readtext('thank-you-in-jpn.jpeg', detail =0, paragraph = False)
+result = reader.readtext('thank-you-in-jpn.jpeg', detail=0, paragraph = False)
 
 # img = cv2.imread('demo-image-jpn.jpeg')
 # result = reader.readtext(img)
@@ -18,5 +22,8 @@ result = reader.readtext('thank-you-in-jpn.jpeg', detail =0, paragraph = False)
 # for detection in result:
 #     print(detection[1])
 
-for detection in result:
-    print(detection)
+for text in result:
+    print(text)
+
+Client = genai.Client(api_key='GEMINI_API_KEY')
+
